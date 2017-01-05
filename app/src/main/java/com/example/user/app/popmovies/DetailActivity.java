1ad16229c0bb58f7e13b1248ca013cabfd527633
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-01-05 11:41:57 +0530

    Solved empty getArguments case in DetailFragment

--- app/src/main/java/com/example/user/app/popmovies/DetailActivity.java
+++ app/src/main/java/com/example/user/app/popmovies/DetailActivity.java
@@ -3,15 +3,18 @@ package com.example.user.app.popmovies;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
 
-    MovieFragment.FetchMovieTask movieTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,25 +22,15 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /**get the movie's Object from the parent activity**/
-        Movie movie = getIntent().getParcelableExtra("currentMovie");
-        Log.v("######", "received title is " + movie.getTitle());
-        Log.v("######", "received overview is " + movie.getOverview());
-        Log.v("######", "received voting is " + movie.getRating());
-        Log.v("######", "received releaseDate is " + movie.getDate());
-
-
-        Bundle movieDetails = new Bundle();
-        movieDetails.putParcelable("DETAIL_MOVIE", movie);
-        Log.v("######", "received movie is " + movie);
-
         if(savedInstanceState == null){
-            DetailFragment defaultMovieFragment = new DetailFragment();
-            defaultMovieFragment.setArguments(movieDetails);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.movie_detail_container, defaultMovieFragment)
-                    .commit();
+            Bundle movieDetails = new Bundle();
+            movieDetails.putParcelable(DetailFragment.DETAIL_MOVIE,
+                    getIntent().getParcelableExtra("currentMovie"));
+            Log.v("######", "received movie is " + getIntent().getParcelableExtra("currentMovie"));
 
+            DetailFragment fragment = new DetailFragment();
+            fragment.setArguments(movieDetails);
+            getSupportFragmentManager().beginTransaction().add(R.id.movie_detail_container,fragment).commit();
         }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -49,6 +42,47 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class DetailFragment extends Fragment {
+
+        public static final String TAG = DetailFragment.class.getSimpleName();
+
+        static final String DETAIL_MOVIE = "DETAIL_MOVIE";
+        Movie mMovie;
+        public DetailFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            TextView titleView = (TextView)rootView.findViewById(R.id.titleView);
+            TextView overviewView = (TextView)rootView.findViewById(R.id.overview);
+            TextView votingView= (TextView)rootView.findViewById(R.id.voteView);
+            TextView releaseDateView = (TextView)rootView.findViewById(R.id.dateView);
+
+
+            Bundle arguments = getArguments();
+            if (arguments != null) {
+                mMovie = arguments.getParcelable(DetailFragment.DETAIL_MOVIE);
+                Log.v("######", "received movie inside fragment is "+mMovie);
+            }
+            if(mMovie!=null){
+                overviewView.setText(mMovie.getOverview());
+                titleView.setText(mMovie.getTitle());
+                votingView.setText(String.valueOf(mMovie.getRating()));
+                releaseDateView.setText(mMovie.getDate());
+            }
+
+            return rootView;
+        }
+    }
+
 
 }
